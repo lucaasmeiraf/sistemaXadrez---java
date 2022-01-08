f288d082dceebf3a49412c6f559b05e57c75dc89
Lucas Meira <lucaas.meiraf@gmail.com>
2022-01-08 20:40:26 -0300

    Começando a impelemntar Tabuleiro e Peça

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,14 +1,12 @@
 package application;
 
-import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Posicao posicao = new Posicao(3, 5);
-
-		System.out.println(posicao);
+		Tabuleiro  tabuleiro = new Tabuleiro(8 ,8);
 		
 	}
 
