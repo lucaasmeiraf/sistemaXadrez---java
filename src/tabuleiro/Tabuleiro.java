03a9132a81da5cc127b49a72e2cbc8a420745698
Lucas Meira <lucaas.meiraf@gmail.com>
2022-01-08 22:53:54 -0300

    Exceções do tabuleiro e programação defensiva

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -7,6 +7,13 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 	
 	public Tabuleiro(int linhas, int colunas) {
+		// PARA CRIAR O TABULEIRO, A QUANTIDADE
+		// DE LINHAS OU COLUNAS PRECISA SER PELO MENOS UM
+		
+		if(linhas < 1 || colunas < 1) {
+			throw new TabuleiroExceptions("Erro ao criar o tabuleiro, é necessário ter pelo menos 1 linha e uma coluna.");
+		}
+		
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -16,33 +23,57 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
-
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
 	
-	//METODO PARA RETORNAR A MATRIZ PEÇA NA LINHA E COLUNA INFORMADA
+	//METODO PARA RETORNAR A MATRIZ PEÇA NA LINHA E COLUNA INFORMADA.
 	public Peca peca(int linha, int coluna) {
+		if(!posicaoExistente(linha, coluna)) {
+			throw new TabuleiroExceptions("Posição fora do tabuleiro!");
+		}
 		return pecas[linha][coluna];
 	}
 	
-	// METODO PARA RETORNAR A PEÇA PELA POSIÇÃO INFORMADA
+	// METODO PARA RETORNAR A PEÇA PELA POSIÇÃO INFORMADA.
 	public Peca peca(Posicao posicao) {
+		if(!posicaoExistente(posicao)) {
+			throw new TabuleiroExceptions("Posição fora do tabuleiro!");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 	
 	// METODO RESPONSÁVEL POR COLOCAR A PEÇA INFORMADA NA POSIÇÃO INFORMADA.
 	
 	public void alocarPeca(Peca peca, Posicao posicao) {
+		if(temUmaPeca(posicao)) {
+			throw new TabuleiroExceptions("Existe uma peça na posição " + posicao + ".");
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
 	
+	
+	/* COMPLEMENTO DO PRÓXIMO MÉTODO, RETORNA UMA CONDIÇÃO V OU F 
+	   CASO A LINHA OU COLUNA INFORMADA SEJA DIFERENTE DO TAMANHO
+	   DO TABULEIRO, RETORNARÁ FALSO.		
+	   
+	   CONDIÇÃO COMPLETA PARA SABER SE ESSA POSIÇÃO EXISTE.
+	*/
+	private boolean posicaoExistente(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+	
+	// PEGA UMA POSIÇÃO E INFORMA SE ESSA POSIÇÃO EXISTE OU NÃO.
+	public boolean posicaoExistente(Posicao posicao) {
+		return posicaoExistente(posicao.getLinha(), posicao.getColuna());
+	}
+	
+	// METODO PARA SABER SE TEM UMA PEÇA NA POSIÇÃO INFORMADA.
+	public boolean temUmaPeca(Posicao posicao) {
+		if(!posicaoExistente(posicao)) {
+			throw new TabuleiroExceptions("Posição fora do tabuleiro!");
+		}
+		return peca(posicao) != null;
+	}
 }
---
+++ src/tabuleiro/TabuleiroExceptions.java
@@ -0,0 +1,11 @@
+package tabuleiro;
+
+public class TabuleiroExceptions extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	
+	public TabuleiroExceptions(String msg) {
+		super(msg);
+	}
+	
+}
